dee01eb39f5332b293cf88df008415c65ce367f7
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-04-19 17:12:08 -0500

    Initial commit

---
+++ src/main/java/com/example/restaurantapiexample/RestaurantApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantApiExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restaurantapiexample/RestaurantApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApiExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
